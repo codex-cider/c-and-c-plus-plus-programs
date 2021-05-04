1238d0010eba94a4b2c2380045e35e63d04a5792
Shubham Chauhan <chauhan.s@sabertoothtech.in>
2021-05-04 21:28:27 +0530

    Data Types

---
+++ data-types.c
@@ -0,0 +1,41 @@
+// Data Types
+
+// What is data
+
+/*
+
+Eg. 
+Mobile No. -- 7854263210
+Name: test user
+Email: codexcider@gmail.com
+Gender: M
+
+Keywords -- 
+
+Data type
+
+Number -- 10, 20, 30 (integer number) -- ( int, long -- keywords)
+Decimal Numbers: -- 10.3, 5.6, -- ( float, double -- keywords )
+Letters -- 'M', 'T', -- ( char -- keywords )
+
+void -- Data type  ( Empty )
+
+Boolean -- True/False ( 0, 1 )
+
+
+*/
+
+#include <stdio.h>
+
+void main()
+{
+    int a;   // Syntaxt -- to create variable ( data_type variable_name );
+    a = 102; // Converting of char to int --
+
+    char ch;
+    ch = 'G';
+
+    float f = 10.25456465;
+
+    printf("Value of a is %d\nValue of ch is %c\nValue of f is %.2f", a, ch, f);
+}

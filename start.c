298183d834aa74a253f31bc78d3c7ab72e79e201
Shubham Chauhan <chauhan.s@sabertoothtech.in>
2021-05-04 15:59:47 +0530

    Variable declaration

--- start.c
+++ start.c
@@ -2,5 +2,11 @@
 
 void main()
 {
-    printf("Called change");
+    printf("C Programing Language\n");
+    printf("This is a new line\n");
+    printf("This is a another printf");
 }
+
+// Keywords
+// Syntax
+// Rules
---
+++ variable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    // Variable
+    int x; // variable declaration
+    // int -- data type (integer) ex. 10, 20, 30
+    // Syntax -- of variable
+    // datatype variable_name;
+    x = 10;
+    printf("x = %d", x);
+}

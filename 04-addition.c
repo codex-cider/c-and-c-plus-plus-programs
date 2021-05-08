7064e03157f72f982208a70fdcdd857edcc17721
Shubham Chauhan <chauhan.s@sabertoothtech.in>
2021-05-08 23:19:50 +0530

    Addition of two integers

---
+++ 04-addition.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+
+    int a, b, sum;
+
+    a = 20;
+    b = 30;
+
+    sum = a + b;
+
+    // printf("SUM is %d", sum);
+
+    printf("%d + %d = %d", a, b, sum);
+    printf("\n");
+    printf("Sum of %d & %d is %d", a, b, sum);
+
+    // 20 + 30 = 50
+}

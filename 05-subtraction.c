cdba3cf54304c680aaf257964892ee97e74fd1a5
Shubham Chauhan <chauhan.s@sabertoothtech.in>
2021-05-08 23:56:58 +0530

    Subtraction of two integers

---
+++ 05-subtraction.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+
+    int firstNumber, secondNumber, subtraction;
+
+    firstNumber = -20;
+    secondNumber = -30;
+
+    subtraction = firstNumber - secondNumber;
+
+    // firstNumber - secondNumber = subtraction
+    printf("%d - %d = %d", firstNumber, secondNumber, subtraction);
+}

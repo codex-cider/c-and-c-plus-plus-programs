22162ad1abcd540f786fbd45384a863b3a0fdc7d
Shubham Chauhan <chauhan.s@sabertoothtech.in>
2021-05-09 22:35:37 +0530

    Multiplication & Square & Cube & Power of X

--- 06-multiplication.c
+++ 06-multiplication.c
@@ -3,8 +3,15 @@
 void main()
 {
 
-    // Square of x
+    int a, b, result;
+    a = 50;
+    b = 30;
+    result = a * b;
+    printf("Result is %d", result);
 
+    printf("\n");
+
+    // Square of x
     int x, square;
     x = 3;
     square = x * x;
@@ -19,7 +26,6 @@ void main()
 
     printf("\n");
     // calculate Power of 4
-    int result;
     x = 5;
     result = x * x * x * x;
     printf("Power 4 of %d is %d", x, result);

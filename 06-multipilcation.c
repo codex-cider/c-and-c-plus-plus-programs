b1d4601d41ea10b3b2be7225797dfaf15cbf37c8
Shubham Chauhan <chauhan.s@sabertoothtech.in>
2021-05-09 22:13:24 +0530

    Multiplication & power calculation

---
+++ 06-multipilcation.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main()
+{
+
+    // Square of x
+
+    int x, square;
+    x = 3;
+    square = x * x;
+    printf("Square of %d is %d", x, square);
+
+    printf("\n");
+
+    int y, cube;
+    y = 3;
+    cube = y * y * y;
+    printf("Cube of %d is %d", y, square);
+
+    printf("\n");
+    // calculate Power of 4
+    int result;
+    x = 5;
+    result = x * x * x * x;
+    printf("Power 4 of %d is %d", x, result);
+}

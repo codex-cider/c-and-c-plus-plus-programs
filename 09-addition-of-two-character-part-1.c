c0dbad00769fc14eac2433b07b128e63059a064f
Shubham Chauhan <chauhan.s@sabertoothtech.in>
2021-05-20 19:40:30 +0530

    addition of two character part 1

---
+++ 09-addition-of-two-character-part-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+    char a;
+    a = 'a';
+
+    int convervalue;
+    convervalue = a;
+
+    printf("Value is %d\n", a);
+    printf("Convervalue is %d", convervalue);
+
+    // char -> int tycasting
+}

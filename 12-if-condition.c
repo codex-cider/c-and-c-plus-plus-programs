52c1fb14d54fb9447f0aa32d9b7160daf1a67cb0
MytraQc <myntraqc@intugine.com>
2021-07-05 08:35:38 +0530

    if-else condition

---
+++ 12-if-condition.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{
+    // if condition
+    if (5 > 10)
+    {
+        printf("5 is greater then 10");
+    }
+    else
+    {
+        printf("5 is not greater then 10");
+    }
+
+    /*
+
+    >, <, >=, <=, ==, != // conditional operator
+
+all conditional operator will return true OR false
+
+    */
+}

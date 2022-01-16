cf4235ed4d01816f0edf28bb2e79811dae9c9662
Krishnakant Madhpuriya <kkmadhpuriya@gmail.com>
2022-01-16 12:41:33 +0530

    Array

---
+++ 15-array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// collection of similar type of data is array
+
+int main()
+{
+
+    int a[5]; // array declaration = data_type variable[size];
+
+    a[0] = 10;
+    a[1] = 20;
+    a[2] = 30;
+    a[3] = 40;
+    a[4] = 50;
+
+    // Print array third element value
+
+    printf("Third element is %d", a[2]);
+}
